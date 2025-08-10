87e009cf84a286c4ab993b2d09c039eef3f71c22
OumObayid <elobayidoumaima@gmail.com>
2025-08-10 12:34:14 +0100

    tache de services terminés

---
+++ src/services/categorieService.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
+
+export function getAllCategories() {
+  return axios.get(`${API_URL}/api/categories/getCategories.php`);
+}
+
+export function getCategorieById(data) {
+  return axios.post(`${API_URL}/api/categories/getCategorieById.php`, data);
+}
+
+export function addCategorie(data) {
+  return axios.post(`${API_URL}/api/categories/addCategorie.php`, data);
+}
+export function updateCategorie(data) {
+  return axios.post(`${API_URL}/api/categories/updateCategorie.php`, data);
+}
+export function deleteCategorie(data) {
+  return axios.post(`${API_URL}/api/categories/deleteCategorie.php`, data);
+}
