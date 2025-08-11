699f1fcb113ccdf1e91b94a9c1a34947bb971853
testobayid <testobayid@gmail.com>
2025-08-11 15:35:27 +0100

    bloqué

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -8,18 +8,22 @@ export const useDataStore = defineStore('dataStore', {
   actions: {   
 
 // ---------------------------------------Gestion des produits
+// Obtenir la liste des produits
     setProducts(products) {
       this.products = products;
     },  
+    //Ajouter un produit
     addProduct(product) {
       this.products.push(product);
     },
+    // Mettre à jour un produit
     updateProduct(updatedProduct) {
       const index = this.products.findIndex(p => p.id === updatedProduct.id);
       if (index !== -1) {
         this.products[index] = { ...this.products[index], ...updatedProduct };
       }
     },
+    // Supprimer un produit
     deleteProduct(productId) {
       this.products = this.products.filter(p => p.id !== productId);
     },
