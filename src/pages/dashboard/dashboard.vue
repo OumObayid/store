80d52e4ea4d4bd219405af16d3ac60bf34f582f1
OumObayid <elobayidoumaima@gmail.com>
2025-08-12 11:42:49 +0100

    correction

--- src/pages/dashboard/dashboard.vue
+++ src/pages/dashboard/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Dashboard</h1>
-        
+            <p>token: {{ token }}</p>
             <p v-if="isLoggedIn">Vous êtes connecté.</p>
             <p>Bonjour: {{ userInfos.firstname }}</p>
             <p>Votre email: {{ userInfos.email }}</p>
@@ -13,9 +13,18 @@
 <script setup>
 import { useAuthStore } from '../../stores/authStore'
 import { useAuth } from '../../composables/useAuth'
+import { useToken } from '../../composables/useToken';
+import { onMounted } from 'vue';
+
+const {token, generateToken}=useToken();
+
 const authStore = useAuthStore();
 const { isLoggedIn, userInfos } = authStore;
 const { logout } = useAuth()
+
+onMounted(()=>{
+    generateToken()
+})
 
 const handlLogout = () => {
     logout();
