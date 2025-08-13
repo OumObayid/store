d9fad5c8111a1e6bb21b81897c9771a6f8561484
OumObayid <elobayidoumaima@gmail.com>
2025-08-13 11:23:38 +0100

    useAuth register terminé

--- src/pages/authPages/VerifyEmail.vue
+++ src/pages/authPages/VerifyEmail.vue
@@ -1,13 +1,37 @@
 <template>
-    <div>
-          Verifyemail
+    <div v-if="loading">
+        Chargement...
+    </div>
+    <div v-else-if="error">
+        {{ error }}
+    </div>
+    <div v-else>
+        {{ message }}
     </div>
 </template>
 
 <script setup>
 
+import { onMounted } from 'vue';
+import { useAuth } from '../../composables/useAuth';
+import { useRoute } from 'vue-router'
+
+const { message, error, loading, verifieEmailConfirmToken } = useAuth();
+
+const route = useRoute()
+const token = route.query.token
+
+const data = {
+    token: token
+}
+
+
+
+onMounted(() => {
+    console.log('token vue:', token);
+
+    verifieEmailConfirmToken(data);
+})
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
