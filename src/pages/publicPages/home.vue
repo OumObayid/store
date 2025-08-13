6bc242c962910449af2db05854022b3cddf1dae6
testobayid <testobayid@gmail.com>
2025-08-13 14:59:49 +0100

    creation de header footer

---
+++ src/pages/publicPages/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+       <h1> home page </h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus exercitationem aliquam consectetur quos officia dolores ea ratione magnam quod? Accusamus enim placeat eaque quas quia aliquid delectus libero eveniet beatae.</p> 
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
